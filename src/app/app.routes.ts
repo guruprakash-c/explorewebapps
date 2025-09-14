0c6ce3ed25dd0eabfef3def186332597bb183d02
Guru Prakash <10187004+guruprakash-c@users.noreply.github.com>
2025-09-14 18:42:48 +0530

    Removed Angular, installing Express JS

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './components/pages/home-page-component/home-page-component.component';
-
-export const routes: Routes = [
-    {
-        path:'',
-        component: HomePageComponent
-    }
-];
